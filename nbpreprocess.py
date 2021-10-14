cafc18cb46a3dc6e85cd134606a954bf897f1300
criddell <criddell@ucdvais.edu>
2021-10-14 14:27:09 -0700

    remove intermediate rst artifact

--- nbpreprocess.py
+++ nbpreprocess.py
@@ -1,5 +1,4 @@
 from nbconvert.preprocessors import Preprocessor
-import nbsphinx
 
 
 class RemoveSlideShowNotesCells(Preprocessor):
@@ -11,25 +10,3 @@ class RemoveSlideShowNotesCells(Preprocessor):
             executable_cells.append(cell)
         notebook.cells = executable_cells
         return notebook, resources
-
-
-class NbSphinxRST(nbsphinx.Exporter):
-    def __init__(self, config=None):
-        super().__init__()
-        self.register_preprocessor(
-            RemoveSlideShowNotesCells(config=config), enabled=True
-        )
-
-    def from_notebook_node(self, nb, resources=None, **kw):
-        rststr, resource = super().from_notebook_node(nb=nb, resources=resources, **kw)
-
-        # Inject dpug metadata variables from notebook into rst representation
-        if "dpug" in getattr(nb, "metadata", {}):
-            dpug_meta = nb.metadata["dpug"]
-            author_str = "*Authored by: {author}, on {date}*\n".format(**dpug_meta)
-
-            rststr = rststr.split("\n", maxsplit=3)
-            rststr.insert(2, author_str)
-            rststr = "\n".join(rststr)
-
-        return rststr, resource
